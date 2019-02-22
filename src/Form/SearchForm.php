5e24e824b3e67b68793db59f5594fd4087c70ad1
Oscar Sanchez <oscar.exe@gmail.com>
2019-02-22 16:38:14 -0600

    adding initial form processing and results in controller

---
+++ src/Form/SearchForm.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scodx
+ * Date: 2019-02-21
+ * Time: 15:50
+ */
+
+namespace Drupal\wiki\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SearchForm extends FormBase {
+
+  public function getFormId() {
+    return 'wiki_search_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['description'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('Search wikipedia from here! No need to go to www.wikipedia.com!'),
+    ];
+
+    $form['query'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search for:'),
+      '#description' => $this->t('What do you want to search?.'),
+      '#required' => TRUE,
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Search'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $query = $form_state->getValue('query');
+    // Simple validation for the query
+    if (strlen(trim($query)) === '') {
+      $form_state->setErrorByName('query', $this->t('The query must not be empty.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $query = $form_state->getValue('query');
+    $form_state->setRedirect('wiki.search_results', ['query' => $query]);
+    return;
+  }
+
+}
